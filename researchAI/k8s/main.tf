7fa2b2312e476ef2ac61c3b648474df2d9c15ed1
Kasaraneni, Geeta Venkata Siva Karthik <kasaranenikarthik@gmail.com>
2025-12-01 16:16:51 -0500

    Added Cloud Composer terraform

---
+++ researchAI/k8s/main.tf
@@ -0,0 +1,62 @@
+provider "google-beta" {
+  project = "karthikdevfest"
+  region  = "us-central1"
+}
+
+resource "google_project_service" "composer_api" {
+    project = "karthikdevfest"
+    provider = google-beta
+    service = "composer.googleapis.com"
+    disable_on_destroy = false
+    check_if_service_has_usage_on_destroy = true
+}
+
+resource "google_service_account" "custom_service_account" {
+    project  = "karthikdevfest"
+    account_id   = "terraform-sa"
+    display_name = "terraform Service Account"
+}
+
+resource "google_project_iam_member" "custom_service_account" {
+  project  = "karthikdevfest"
+  member   = format("serviceAccount:%s", google_service_account.custom_service_account.email)
+  role     = "roles/composer.worker"
+}
+
+resource "google_composer_environment" "example_environment" {
+  name = "rag-environment"
+  provider = google-beta
+
+  depends_on = [
+    google_project_iam_member.custom_service_account,
+    google_project_service.composer_api
+  ]
+
+  config {
+
+    software_config {
+      image_version = "composer-3-airflow-3.1.0-build.2"
+      pypi_packages = {
+        pandas                        = ""
+        scikit-learn                  = ""
+        torch                          = ""
+        transformers                   = ""
+        sentencepiece                  = ""
+        kneed                          = ""
+        requests                       = ""
+        apache-airflow-providers-postgres = ""
+        great-expectations             = "0.18.8"
+        sqlalchemy                     = ""
+        beautifulsoup4                 = ""
+        lxml                           = ""
+        html5lib                       = ""
+        fairlearn                      = ""
+        "dvc[gs]"                      = ""
+      }
+    }
+
+    node_config {
+      service_account = google_service_account.custom_service_account.email
+    }
+  }
+}

df9e6adf4c1f5b7057779dd657bf9ea42c2b0c35
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-12-06 19:26:31 -0500

    terraform fix

--- researchAI/k8s/terraform.sh
+++ researchAI/k8s/terraform.sh
@@ -1,4 +1,4 @@
 terraform import google_composer_environment.example_environment "projects/mlops-gcp-lab1/locations/us-east1/environments/rag-environment"
 terraform import google_service_account.custom_service_account "projects/mlops-gcp-lab1/serviceAccounts/terraform-sa@mlops-gcp-lab1.iam.gserviceaccount.com"
-terraform import google_storage_bucket.composer_logs_bucket "projects/mlops-gcp-lab1/locations/us-east1/buckets/composer-logs-bucket-mlops-gcp-lab1"
+terraform import google_storage_bucket.composer_logs_bucket "projects/mlops-gcp-lab1/locations/us-east1/buckets/airflow-logs-bucket-mlops-gcp-lab1"
 terraform import google_logging_project_sink.composer_logs_sink "projects/mlops-gcp-lab1/sinks/airflow-logs-sink"
